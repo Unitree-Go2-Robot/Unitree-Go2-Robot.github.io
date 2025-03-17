dd6382784deaa333d6f35943dc9eeea08e865a64
Juancams <juancarlos.serrano@urjc.es>
2025-03-17 16:31:41 +0100

    [skip ci] publish latest
    Signed-off-by: Juancams <juancarlos.serrano@urjc.es>

--- _static/tcs_theme.js
+++ _static/tcs_theme.js
@@ -2,14 +2,12 @@ var i;
 var contents = document.getElementsByClassName("content-collapse section");
 
 for (i = 0; i < contents.length; i++) {
-  // Asegúrate de que la clase "content-collapse section" esté ocurriendo en un <div>
   if (contents[i].tagName.toLowerCase() == 'div') {
     var element = contents[i].children[0];
     var element_type = element.tagName.toLowerCase();
     var span_id;
     var spanElement;
 
-    // Si el siguiente elemento es un span, obtener el id y continuar con el header
     if (element_type == 'span') {
       span_id = element.id;
       element.id = "";
@@ -18,32 +16,45 @@ for (i = 0; i < contents.length; i++) {
     }
 
     var btn = document.createElement("BUTTON");
-    // Si es un header, captura el nivel y pasa al botón
     if (element_type.length == 2 && element_type[0] == 'h') {
       var newClass = 'clps' + element_type[1];
-      // Colapsa la sección por defecto si JavaScript está habilitado
       contents[i].style.maxHeight = 0;
-      // Construye el botón y define su comportamiento
+
       btn.className += " " + newClass;
       btn.innerHTML = element.innerHTML;
       btn.className += " collapsible";
       btn.id = span_id;
+      btn.setAttribute('aria-expanded', 'false');
+
+      // Restablecer el estado de expansión/collapsado
+      var savedState = localStorage.getItem(span_id);
+      if (savedState) {
+        btn.setAttribute('aria-expanded', savedState);
+        if (savedState === 'true') {
+          contents[i].style.maxHeight = contents[i].scrollHeight + "px";
+        }
+      }
+
       btn.addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.maxHeight != "0px") {
+        var isExpanded = this.getAttribute('aria-expanded') === 'true';
+
+        // Cambiar el valor de aria-expanded
+        this.setAttribute('aria-expanded', !isExpanded);
+
+        if (isExpanded) {
           content.style.maxHeight = 0;
         } else {
           content.style.maxHeight = content.scrollHeight + "px";
         }
+
+        // Guardar el estado en localStorage
+        localStorage.setItem(this.id, !isExpanded);
       });
 
-      // Añade el botón a la página y elimina el header
       contents[i].parentNode.insertBefore(btn, contents[i]);
       contents[i].removeChild(element);
-    } else {
-      // Restablece el id del span si no está seguido por un elemento Hx
-      spanElement.id = span_id;
     }
   }
 }
@@ -64,7 +75,6 @@ function addVersionDropdown(versions) {
   var currentVersionText = document.createElement("span");
   currentVersionText.className = "current-version";
 
-  // Obtener la versión actual desde el almacenamiento local o usar "Humble" como predeterminada
   var currentVersion = localStorage.getItem("ros2_version") || "Humble";
   currentVersionText.innerHTML = " (current: " + currentVersion + ")";
 
@@ -90,19 +100,15 @@ function addVersionDropdown(versions) {
     versionLink.style.color = "black";
 
     versionLink.addEventListener("click", function(e) {
-      // Prevenir la acción predeterminada del enlace para evitar la recarga instantánea
       e.preventDefault();
-
-      // Guardar la versión actual en el almacenamiento local
       localStorage.setItem("ros2_version", version.name);
       currentVersionText.innerHTML = " (current: " + version.name + ")";
       versionList.style.maxHeight = "0";
-      updateSidebarLinks(version.name);  // Actualizar enlaces al cambiar de versión
+      updateSidebarLinks(version.name);
 
-      // Redirigir a la nueva URL después de que el DOM se actualice
       setTimeout(function() {
         window.location.href = version.url;
-      }, 200);  // Retardo para que se actualicen los enlaces antes de redirigir
+      }, 200);
     });
 
     versionList.appendChild(versionLink);
@@ -133,13 +139,12 @@ function updateSidebarLinks(version) {
     var url = new URL(link.href);
     var pathParts = url.pathname.split('/');
 
-    // Cambiar la versión solo en los enlaces que no sean 'index.html' para mantener la estructura
     if (version === "Humble") {
       if (url.pathname !== '/index.html' && !url.pathname.includes("humble")) {
-        url.pathname = '/index.html';  // Regresa a la página principal para 'Humble'
+        url.pathname = '/index.html';
       }
     } else if (version !== "Humble" && !url.pathname.includes(version.toLowerCase())) {
-      pathParts[1] = version.toLowerCase();  // Cambiar la versión en el path
+      pathParts[1] = version.toLowerCase();
       url.pathname = pathParts.join('/');
     }
 
